ad3eef68081f6ba779241520c82127dbf7871ebb
Flatiron School <email.the.anderson@gmail.com>
2019-08-03 14:19:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  let result;
   // Write your code here!
+  if (distance <= 400){
+    result = "This one is on me!"
+  }
+  else if (distance > 2000) {
+    if (distance < 2500) {
+      result = "I will gladly take your thirty bucks."
+    }
+    else if (distance > 2500) {
+      result = "No can do." 
+    }
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result; 
+  city == 'NYC' ? (result = "Ok, sounds good.") : (result = "No go.")
+  return result 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.'
+      break;
+    default:
+      result = 'Bye.'
+      break;
+  }
+  return result 
 }
