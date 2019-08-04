c5d8138432bf36e75fa0c2fca1fc84a19175601c
yoseflehrer <yolehrer@gmail.com>
2019-08-04 02:18:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  let res
+  if (num < 400) {
+    res = 'This one is on me!';
+  } else if (1999 < num && num < 2500){
+    res = 'I will gladly take your thirty bucks.';
+  } else {
+    res = 'No can do.';
+  }
+  return res
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(n){
   // Write your code here!
+  return (n === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let res
+  switch (tip) {
+    case 'generous':
+      res = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      res = 'Thank you.'
+      break;
+    case 'thanks for everything':
+      res = 'Bye.'
+    default:
+      break;
+  }
+  return res
 }
